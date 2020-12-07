bfa8b8e44f309d9ee2b322dcecb457e13ada1926
antonio <antonio.badan@gmail.com>
2020-12-07 20:52:03 +0100

    correzione close #5

--- src/main/java/it/unipd/tos/business/TakeAwayGelateria.java
+++ src/main/java/it/unipd/tos/business/TakeAwayGelateria.java
@@ -19,7 +19,7 @@ public class TakeAwayGelateria implements TakeAwayBill{
         }
 
         if(itemsOrdered.contains(null)) {
-            throw new TakeAwayBillException
+           throw new TakeAwayBillException
                 ("La lista contiene un elemento nullo"); 
         }
         
@@ -27,16 +27,19 @@ public class TakeAwayGelateria implements TakeAwayBill{
         double gelatoMenoCaro=0.00;
         
         for(MenuItem item : itemsOrdered) {
-        	if (item.getItemType() == MenuItem.item.Gelati) {
-        		if (numGelati>0 && item.getPrice() < gelatoMenoCaro )
-        			gelatoMenoCaro=item.getPrice();
-        		else
-        			gelatoMenoCaro=item.getPrice();
-        		numGelati++;
-        	}
+            if (item.getItemType() == MenuItem.item.Gelati) {
+                if (numGelati>0 && item.getPrice() < gelatoMenoCaro ) {
+                    gelatoMenoCaro=item.getPrice();
+                }
+                else {
+                    gelatoMenoCaro=item.getPrice();
+                }
+                numGelati++;
+            }
             totalPrice += item.getPrice();
-            if (numGelati > 5)
-            	totalPrice -= gelatoMenoCaro*0.5;
+            if (numGelati > 5) {
+                totalPrice -= gelatoMenoCaro*0.5;
+            }
         }
 
         return totalPrice;
